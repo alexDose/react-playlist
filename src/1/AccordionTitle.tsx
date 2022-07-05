d5ee1cad48201551f333bc948140a6d4ee02451a
Alexei <i0ver@mail.ru>
2022-07-05 21:18:11 +0300

    first commit

---
+++ src/1/AccordionTitle.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Accordion = () => {
+    return (
+        <div>
+            <h3>menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
